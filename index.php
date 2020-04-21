14caf3a2a3d807eb9c8ce1000dca9f5eee13357c
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-04-21 19:21:03 -0300

    .

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+ob_start();
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Source\Core\Session;
+use CoffeeCode\Router\Router;
+
+$session = new Session();
+$route = new Router(url(), ":");
+
+
+//
+// ─── ROUTES ──────────────────────────────────────────────────────────────────────
+//
+
+$route->namespace("Source\Controllers");
+$route->get("/", "Web:index");
+
+$route->namespace("Source\Controllers")->group("/error");
+$route->get("/{errcode}", "Web:error");
+
+//
+// ─── DISPATCH ───────────────────────────────────────────────────────────────────
+//
+
+$route->dispatch();
+
+if($route->error()){
+    $route->redirect("/error/{$route->error()}");
+}
+
+ob_end_flush();
