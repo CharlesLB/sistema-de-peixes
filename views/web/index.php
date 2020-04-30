c80e1a3ff017cc5c16996c3e61f9033f7b43ae6b
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-04-30 10:14:54 -0300

    .

--- views/web/index.php
+++ views/web/index.php
@@ -1,105 +1,179 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Hyperspace by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/css/globals.css">
-    <title>Sistema de Peixes</title>
-</head>
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Bem Vindo!</a></li>
+							<li><a href="#one">Nosso Projeto</a></li>
+							<li><a href="#two">O que fazemos</a></li>
+							<li><a href="#three">Área Restrita</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
 
-<body>
-    
-    <div class="home">
-        <div class="landing-text">
-            <h1>Laboratório de Biologia</h1>
-            <h3>Aqui a Viviane deverá escrever um textinho legal ou retirar este h3</h3>
-        </div>
-    </div>
+		<!-- Wrapper -->
+			<div id="wrapper">
 
-    <section id="intro">
-        <div class="padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <img src="../assets/img/web/intro.png" alt="example">
-                    </div>
-                    <div class="col-sm-6 text-center">
-                        <h2>Nosso Projeto</h2>
-                        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus itaque
-                            magnam quas. Maxime, ratione! Maxime tempore unde assumenda repudiandae dolore iusto aut
-                            voluptatem! Excepturi sed dignissimos odio hic laudantium sapiente.</p>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore assumenda
-                            cumque ex optio, porro alias amet deserunt praesentium, dolorem nisi maiores omnis
-                            repudiandae reprehenderit at itaque magnam, sequi quis. Id.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Projeto de Biologia</h1>
+							<p>Um projeto pertencente ao <a href="https://www.ifsudestemg.edu.br/juizdefora">IF Sudeste MG Campus Juiz de Fora</a><br />
+							com o objetivo de esturdarmos peixes bacanas</a>.</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Conhecer nosso projeto</a></li>
+							</ul>
+						</div>
+					</section>
 
-    <section id="login">
-        <div class="padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <img src="../assets/img/web/intro.png" alt="example">
-                    </div>
-                    <div class="col-sm-6 d-flex align-items-center">
-                        <form>
-                            <h2>Login</h2>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Matrícula</label>
-                                <input type="number" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Senha</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                            <button type="submit" class="btn button">Login</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="assets/images/pic02.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Nosso projeto é legal</h2>
+									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="assets/images/pic01.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Feugiat consequat</h2>
+									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="assets/images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Ultricies aliquam</h2>
+									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+								</div>
+							</div>
+						</section>
+					</section>
 
-    <footer>
-        <div class="footer" id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                        <h4> Laboratório de Biologia </h4>
-                    </div>
-                    <div class="col-lg-3 col-sm-2 col-xs-3">
-                        <h3> Contact </h3>
-                        <ul>
-                            <li><a class="email" href="#">laboratoriobiologiaif@gmail.com</a></li>
-                            <br />
-                            <li>
-                                <p> IF Sudeste MG Campus Juiz de Fora</p>
-                            </li>
-                            <li>
-                                <p> R. Bernardo Mascarenhas, 1283 - Fábrica, Juiz de Fora - MG, 36080-001</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom">
-                <div class="container">
-                    <p class="pull-left copyright"> <a href="https://github.com/CharlesLB" target="_blank">@CharlesLB </a> e Viviane Serafim </p>
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>O que nós fazemos</h2>
+							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>Lorem ipsum amet</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>Aliquam sed nullam</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>Sed erat ullam corper</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-desktop"></span>
+									<h3>Veroeros quis lorem</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-link"></span>
+									<h3>Urna quis bibendum</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon major fa-gem"></span>
+									<h3>Aliquam urna dapibus</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+							</div>
+						</div>
+					</section>
 
-                </div>
-            </div>
-    </footer>
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Área Restrita</h2>
+							<p>Caso seja um bolsista ou voluntário, basta fazer seu login para prosseguir. Caso ainda não possui um login, entre em contato com o orientador do projeto.</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="id">Matrícula</label>
+												<input type="number" name="id" id="id" />
+											</div>
+											<div class="field half">
+												<label for="Senha">Senha</label>
+												<input type="password" name="password" id="password" />
+											</div>
+										</div>
+										<ul class="actions">
+											<li><button type="submit" class="button submit">Login</button></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Endereço</h3>
+											<span> IF Sudeste MG Campus Juiz de Fora<br />
+											R. Bernardo Mascarenhas, 1283 - Fábrica, Juiz de Fora - MG, 36080-001<br />
+											Bloco C</span>
+										</li>
+										<li>
+											<h3>E-mail</h3>
+											<a href="#">laboratoriobiologiaif@gmail.com</a>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</body>
+			</div>
 
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>Projeto desenvolvido por Viviane Serafim e <a href="https://github.com/CharlesLB/sistema-de-peixes">@CharlesLB</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
