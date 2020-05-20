a0b2947d24c5a651671d1f186062c6b35a1a76fa
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-20 11:09:06 -0300

    Global Variables

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,110 +1,96 @@
 <?php
-
-//
-// ─── DATABASE ───────────────────────────────────────────────────────────────────
-//
-    
+/**
+ * DATABASE
+ */
 define("CONF_DB_HOST", "localhost");
 define("CONF_DB_USER", "root");
 define("CONF_DB_PASS", "");
-define("CONF_DB_NAME", "db_peixes");
+define("CONF_DB_NAME", "fullstackphp");
 
-//
-// ─── URL ────────────────────────────────────────────────────────────────────────
-//
-
-    
-define("CONF_URL_BASE", "https://www.sistemadepeixes.com");
-define("CONF_URL_TEST", "https://www.localhost/sistema-de-peixes");
+/**
+ * PROJECT URLs
+ */
+define("CONF_URL_BASE", "https://www.cafecontrol.com.br");
+define("CONF_URL_TEST", "https://www.localhost/fsphp");
 define("CONF_URL_ADMIN", "/admin");
 
-
-//
-// ─── SITE ───────────────────────────────────────────────────────────────────────
-//
-
-    
-define("CONF_SITE_NAME", "Laboratório de Biologia");
-define("CONF_SITE_TITLE", "Site da bolsa de biologia do IF Sudeste MG Campus Juiz de Fora");
+/**
+ * SITE
+ */
+define("CONF_SITE_NAME", "CaféControl");
+define("CONF_SITE_TITLE", "Gerencie suas contas com o melhor café");
 define("CONF_SITE_DESC",
-    "O que o laboratório faz. Aqui é um pequeno textinho que aparece quando enviar o link para alguém kk");
+    "O CafeControl é um gerenciador de contas simples, poderoso e gratuito. O prazer de tomar um café e ter o controle total de suas contas.");
 define("CONF_SITE_LANG", "pt_BR");
-define("CONF_SITE_DOMAIN", "LINK.COM");
-define("CONF_SITE_ADDR_STREET", "R. Bernardo Mascarenhas");
-define("CONF_SITE_ADDR_NUMBER", "1283");
-define("CONF_SITE_ADDR_COMPLEMENT", "IF Sudeste MG Campus Juiz de Fora.");
-define("CONF_SITE_ADDR_CITY", "Juiz de Fora");
-define("CONF_SITE_ADDR_STATE", "MG");
-define("CONF_SITE_ADDR_ZIPCODE", "36080-001");
+define("CONF_SITE_DOMAIN", "upinside.com.br");
 
+/**
+ * SOCIAL
+ */
+define("CONF_SOCIAL_TWITTER_CREATOR", "@robsonvleite");
+define("CONF_SOCIAL_TWITTER_PUBLISHER", "@robsonvleite");
+define("CONF_SOCIAL_FACEBOOK_APP", "626590460695980");
+define("CONF_SOCIAL_FACEBOOK_PAGE", "upinside");
+define("CONF_SOCIAL_FACEBOOK_AUTHOR", "robsonvleiteoficial");
+define("CONF_SOCIAL_GOOGLE_PAGE", "107305124528362639842");
+define("CONF_SOCIAL_GOOGLE_AUTHOR", "103958419096641225872");
+define("CONF_SOCIAL_INSTAGRAM_PAGE", "robsonvleite");
+define("CONF_SOCIAL_YOUTUBE_PAGE", "upinside");
 
-//
-// ─── DATE ───────────────────────────────────────────────────────────────────────
-//
-  
+/**
+ * DATES
+ */
 define("CONF_DATE_BR", "d/m/Y H:i:s");
 define("CONF_DATE_APP", "Y-m-d H:i:s");
 
-
-//
-// ─── PASSWORD ───────────────────────────────────────────────────────────────────
-//
-
+/**
+ * PASSWORD
+ */
 define("CONF_PASSWD_MIN_LEN", 8);
 define("CONF_PASSWD_MAX_LEN", 40);
 define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
 define("CONF_PASSWD_OPTION", ["cost" => 10]);
 
-//
-// ─── MESSAGE ────────────────────────────────────────────────────────────────────
-//
-
-    
+/**
+ * MESSAGE
+ */
 define("CONF_MESSAGE_CLASS", "trigger");
 define("CONF_MESSAGE_INFO", "info");
 define("CONF_MESSAGE_SUCCESS", "success");
 define("CONF_MESSAGE_WARNING", "warning");
 define("CONF_MESSAGE_ERROR", "error");
 
-//
-// ─── VIEW ───────────────────────────────────────────────────────────────────────
-//
-
-    
+/**
+ * VIEW
+ */
 define("CONF_VIEW_PATH", __DIR__ . "/../../shared/views");
 define("CONF_VIEW_EXT", "php");
-define("CONF_VIEW_THEME", "Admin");
+define("CONF_VIEW_THEME", "cafecontrol");
 
-
-//
-// ─── UPLOAD ─────────────────────────────────────────────────────────────────────
-//
-
-
+/**
+ * UPLOAD
+ */
 define("CONF_UPLOAD_DIR", "../storage");
 define("CONF_UPLOAD_IMAGE_DIR", "images");
 define("CONF_UPLOAD_FILE_DIR", "files");
 define("CONF_UPLOAD_MEDIA_DIR", "medias");
 
-//
-// ─── IMAGES ─────────────────────────────────────────────────────────────────────
-//
-
-
+/**
+ * IMAGES
+ */
 define("CONF_IMAGE_CACHE", CONF_UPLOAD_DIR . "/" . CONF_UPLOAD_IMAGE_DIR . "/cache");
 define("CONF_IMAGE_SIZE", 2000);
 define("CONF_IMAGE_QUALITY", ["jpg" => 75, "png" => 5]);
 
-//
-// ─── MAIL ───────────────────────────────────────────────────────────────────────
-//
-
-
-define("CONF_MAIL_HOST", "");
-define("CONF_MAIL_PORT", "");
-define("CONF_MAIL_USER", "");
-define("CONF_MAIL_PASS", "");
-define("CONF_MAIL_SENDER", ["name" => "Laboratório Biologia", "address" => "laboratoriobiologiaifjf@gmail.com"]);
+/**
+ * MAIL
+ */
+define("CONF_MAIL_HOST", "smtp.sendgrid.net");
+define("CONF_MAIL_PORT", "587");
+define("CONF_MAIL_USER", "apikey");
+define("CONF_MAIL_PASS", "**************************");
+define("CONF_MAIL_SENDER", ["name" => "Robson V. Leite", "address" => "cursos@upinside.com.br"]);
+define("CONF_MAIL_SUPPORT", "cursos@upinside.com.br");
 define("CONF_MAIL_OPTION_LANG", "br");
 define("CONF_MAIL_OPTION_HTML", true);
 define("CONF_MAIL_OPTION_AUTH", true);
