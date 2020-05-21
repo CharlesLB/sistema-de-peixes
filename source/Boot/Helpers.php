4032d6939c7699f4941e866575633408a8ec6ac1
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-21 11:16:18 -0300

    General Functions

--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -1,215 +1,57 @@
 <?php
 
-/**
- * ####################
- * ###   VALIDATE   ###
- * ####################
- */
-
-function is_email(string $email): bool
-{
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-
-function is_passwd(string $password): bool
-{
-    if (password_get_info($password)['algo']) {
-        return true;
-    }
-
-    return (mb_strlen($password) >= CONF_PASSWD_MIN_LEN && mb_strlen($password) <= CONF_PASSWD_MAX_LEN ? true : false);
-}
-
-/**
- * ##################
- * ###   STRING   ###
- * ##################
- */
-
-function str_slug(string $string): string
-{
-    $string = filter_var(mb_strtolower($string), FILTER_SANITIZE_STRIPPED);
-    $formats = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]/?;:.,\\\'<>°ºª';
-    $replace = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr                                 ';
-
-    $slug = str_replace(["-----", "----", "---", "--"], "-",
-        str_replace(" ", "-",
-            trim(strtr(utf8_decode($string), utf8_decode($formats), $replace))
-        )
-    );
-    return $slug;
-}
-
-function str_studly_case(string $string): string
-{
-    $string = str_slug($string);
-    $studlyCase = str_replace(" ", "",
-        mb_convert_case(str_replace("-", " ", $string), MB_CASE_TITLE)
-    );
-
-    return $studlyCase;
-}
-
-function str_camel_case(string $string): string
-{
-    return lcfirst(str_studly_case($string));
-}
-
-function str_title(string $string): string
-{
-    return mb_convert_case(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS), MB_CASE_TITLE);
-}
-
-function str_limit_words(string $string, int $limit, string $pointer = "..."): string
-{
-    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
-    $arrWords = explode(" ", $string);
-    $numWords = count($arrWords);
-
-    if ($numWords < $limit) {
-        return $string;
-    }
-
-    $words = implode(" ", array_slice($arrWords, 0, $limit));
-    return "{$words}{$pointer}";
-}
-
-function str_limit_chars(string $string, int $limit, string $pointer = "..."): string
-{
-    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
-    if (mb_strlen($string) <= $limit) {
-        return $string;
-    }
-
-    $chars = mb_substr($string, 0, mb_strrpos(mb_substr($string, 0, $limit), " "));
-    return "{$chars}{$pointer}";
-}
-
-/**
- * ###############
- * ###   URL   ###
- * ###############
- */
+//
+// ─── ROUTES ─────────────────────────────────────────────────────────────────────
+//
 
 function url(string $path = null): string
 {
     if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
         if ($path) {
-            return CONF_URL_TEST . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+            return URL["localhost"]  . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
         }
-        return CONF_URL_TEST;
+        return URL["localhost"] ;
     }
 
     if ($path) {
-        return CONF_URL_BASE . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+        return URL["base"] . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
     }
 
-    return CONF_URL_BASE;
+    return URL["base"] ;
 }
 
-function url_back(): string
-{
-    return ($_SERVER['HTTP_REFERER'] ?? url());
-}
-
-function theme(string $path = null): string
+function asset(string $path, string $view): ?string
 {
     if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
-        if ($path) {
-            return CONF_URL_TEST . "/themes/" . CONF_VIEW_THEME . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
-        }
-
-        return CONF_URL_TEST . "/themes/" . CONF_VIEW_THEME;
+        return URL["localhost"] . "/views/{$view}/assets/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
     }
 
-    if ($path) {
-        return CONF_URL_BASE . "/themes/" . CONF_VIEW_THEME . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+    return URL["base"] . "/views/{$view}/assets/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+}
+
+function fragment(string $path, string $view): ?string
+{
+    if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
+        return URL["localhost"] . "/views/{$view}/fragments/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
     }
 
-    return CONF_URL_BASE . "/themes/" . CONF_VIEW_THEME;
+    return URL["base"] . "/views/{$view}/fragments/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
 }
 
-function redirect(string $url): void
+function shared(string $path): ?string
 {
-    header("HTTP/1.1 302 Redirect");
-    if (filter_var($url, FILTER_VALIDATE_URL)) {
-        header("Location: {$url}");
-        exit;
+    if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
+        return URL["localhost"] . "/Shared/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
     }
 
-    if (filter_input(INPUT_GET, "route", FILTER_DEFAULT) != $url) {
-        $location = url($url);
-        header("Location: {$location}");
-        exit;
-    }
+    return URL["base"] . "/Shared/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
 }
 
-/**
- * ################
- * ###   DATE   ###
- * ################
- */
+//
+// ─── MESSAGE ────────────────────────────────────────────────────────────────────
+//
 
-function date_fmt(string $date = "now", string $format = "d/m/Y H\hi"): string
+function message(string $message, string $type): string
 {
-    return (new DateTime($date))->format($format);
-}
-
-function date_fmt_br(string $date = "now"): string
-{
-    return (new DateTime($date))->format(CONF_DATE_BR);
-}
-
-function date_fmt_app(string $date = "now"): string
-{
-    return (new DateTime($date))->format(CONF_DATE_APP);
-}
-
-/**
- * ####################
- * ###   PASSWORD   ###
- * ####################
- */
-
-function passwd(string $password): string
-{
-    return password_hash($password, CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
-}
-
-/**
- * @param string $password
- * @param string $hash
- * @return bool
- */
-function passwd_verify(string $password, string $hash): bool
-{
-    return password_verify($password, $hash);
-}
-
-function passwd_rehash(string $hash): bool
-{
-    return password_needs_rehash($hash, CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
-}
-
-/**
- * ################
- * ###   CSRF   ###
- * ################
- */
-
-function csrf_input(): string
-{
-    $session = new \Source\Core\Session();
-    $session->csrf();
-    return "<input type='hidden' name='csrf' value='" . ($session->csrf_token ?? "") . "'/>";
-}
-
-function csrf_verify($request): bool
-{
-    $session = new \Source\Core\Session();
-    if (empty($session->csrf_token) || empty($request['csrf']) || $request['csrf'] != $session->csrf_token) {
-        return false;
-    }
-    return true;
+    return "<div class=\"message {$type}\">{$message}</div>";
 }
