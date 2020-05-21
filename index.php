012ffc990cb6a918fbfde4e66da7204f0ee4401a
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-21 11:18:46 -0300

    Routes

--- index.php
+++ index.php
@@ -1,36 +1,67 @@
 <?php
-ob_start();
+
+ob_flush();
 
 require __DIR__ . "/vendor/autoload.php";
 
-use Source\Core\Session;
 use CoffeeCode\Router\Router;
 
-$session = new Session();
-$route = new Router(url(), ":");
-
-
-//
-// ─── ROUTES ──────────────────────────────────────────────────────────────────────
-//
+$route = new Router(url());
 
 $route->namespace("Source\Controllers");
-$route->get("/", "Web:index");
-
-$route->namespace("Source\Controllers")->group("/admin");
-$route->get("/", "Admin:index");
-
-$route->namespace("Source\Controllers")->group("/error");
-$route->get("/{errcode}", "Web:error");
 
 //
-// ─── DISPATCH ───────────────────────────────────────────────────────────────────
+// ─── WEB ────────────────────────────────────────────────────────────────────────
+//
+
+
+$route->group(null);
+$route->get("/", "Web:home", "web.home");
+$route->post("/contato", "Mail:contact", "mail.contact");
+
+$route->get("/login", "Web:login", "web.login");
+$route->get("/esqueci-senha", "Web:forget", "web.forget");
+$route->get("/nova-senha", "Web:newPassword", "web.newPassword");
+
+
+
+//
+// ─── ADMIN ──────────────────────────────────────────────────────────────────────
+//
+
+    
+$route->group("/admin");
+$route->get("/", "Web:admin", "web.admin");
+
+$route->get("/categoria","Web:category", "web.category");
+$route->post("/create-category", "CategoryController:create", "category.create");
+$route->post("/delete-category", "CategoryController:delete", "category.delete");
+
+$route->get("/palavra", "Web:word", "web.word");
+$route->post("/create-word", "WordController:create", "word.create");
+$route->post("/delete-word", "WordController:delete", "word.delete");
+$route->post("/show-word", "WordController:show", "word.show");
+
+
+
+//
+// ─── ERROR ──────────────────────────────────────────────────────────────────────
+//
+
+$route->group("error");
+$route->get("/{errcode}", "Web:error", "web.error");
+
+//
+// ─── PROCESS ────────────────────────────────────────────────────────────────────
 //
 
 $route->dispatch();
 
-if($route->error()){
+if ($route->error()){
     $route->redirect("/error/{$route->error()}");
 }
 
+
+
 ob_end_flush();
+
