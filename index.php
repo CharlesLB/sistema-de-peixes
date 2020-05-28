d17ca95ddba658c495195b4f0fb81f527d7d37ad
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-05-28 16:49:16 -0300

    Routes

--- index.php
+++ index.php
@@ -19,12 +19,18 @@ $route->group(null);
 $route->get("/", "Web:home", "web.home");
 $route->post("/contato", "Mail:contact", "mail.contact");
 
-$route->get("/login", "Web:login", "web.login");
-$route->get("/esqueci-senha", "Web:forget", "web.forget");
-$route->get("/confirmar-codigo", "Web:code", "web.code");
+
+//
+// ─── AUTH ───────────────────────────────────────────────────────────────────────
+//
+
+$route->group(null);
+$route->get("/login", "Web:login", "Web.login");
+$route->post("/login", "Auth:login", "auth.login");
+$route->get("/esqueci-senha", "Web:forget", "Web.forget");
+$route->post("/esqueci-senha", "Auth:forget", "auth.forget");
 $route->get("/nova-senha", "Web:newPassword", "web.newPassword");
-
-
+$route->post("/nova-senha", "Auth:newPassword", "auth.newPassword");
 
 //
 // ─── ADMIN ──────────────────────────────────────────────────────────────────────
@@ -36,11 +42,12 @@ $route->get("/", "Web:admin", "web.admin");
 
 $route->get("/especies","Web:species", "web.species");
 $route->post("/create-specie", "SpecieController:create", "specie.create");
+$route->put("/edit-specie", "SpecieController:edit", "specie.edit");
 $route->post("/delete-specie", "SpecieController:delete", "category.delete");
 
 $route->get("/peixe/{specie}", "Web:fishes", "web.fishes");
 $route->post("/create-fish", "FishController:create", "word.create");
-$route->post("/edit-fish", "FishController:edit", "word.edit");
+$route->put("/edit-fish", "FishController:edit", "word.edit");
 $route->post("/delete-word", "FishController:delete", "word.delete");
 
 
