f787b1fddd6716540d060cfed1970a744080e46a
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-05-28 16:48:10 -0300

    Models

--- source/Models/Fish.php
+++ source/Models/Fish.php
@@ -3,6 +3,7 @@
 namespace Source\Models;
 
 use CoffeeCode\DataLayer\DataLayer;
+use Exception;
 
 class Fish extends DataLayer
 {
@@ -10,4 +11,36 @@ class Fish extends DataLayer
     {
         parent::__construct("fishes", []);
     }
+
+    public function validate(): bool
+    {
+        if (empty($this->name)){
+            $this->fail = new Exception("Insira o nome da espécie!");
+            return false;
+        }
+
+        if($this->findByName()){
+            $this->fail = new Exception("Categoria já cadastrada!");
+            return false;
+        }
+            
+        return true;
+    }
+
+    //
+    // ─── PRIVATE FUNCTIONS ──────────────────────────────────────────────────────────
+    //
+
+    private function findByName(): ?object
+    {
+        $fishByName = $this->find("name = :name", "name={$this->name}")->count();
+
+        if($fishByName){
+            return $fishByName;
+        }
+
+        return null;
+    }
+    
+    
 }
--- source/Models/Specie.php
+++ source/Models/Specie.php
@@ -3,11 +3,42 @@
 namespace Source\Models;
 
 use CoffeeCode\DataLayer\DataLayer;
+use Exception;
 
 class Specie extends DataLayer
 {
     public function __construct()
     {
         parent::__construct("species", ["name"]);
+    }
+
+    public function validate(): bool
+    {
+        if (empty($this->name)){
+            $this->fail = new Exception("Insira o nome da espécie!");
+            return false;
+        }
+
+        if($this->findByName()){
+            $this->fail = new Exception("Categoria já cadastrada!");
+            return false;
+        }
+            
+        return true;
+    }
+
+    //
+    // ─── PRIVATE FUNCTIONS ──────────────────────────────────────────────────────────
+    //
+
+    private function findByName(): ?object
+    {
+        $specieByName = $this->find("name = :name", "name={$this->name}")->count();
+
+        if($specieByName){
+            return $specieByName;
+        }
+
+        return null;
     }
 }
