2e57972c4cbb0af43689918735adde2b298d735d
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-05-28 16:48:30 -0300

    Toutes Controller

--- source/Controllers/Web.php
+++ source/Controllers/Web.php
@@ -20,30 +20,25 @@ class Web extends Controller
         ]);
     }
 
-    // ─── LOGIN ──────────────────────────────────────────────────────────────────────
+    //
+    // ─── AUTH ───────────────────────────────────────────────────────────────────────
+    //
 
-    public function login()
+    public function login(): void
     {       
         echo $this->view->render("login/login" , [
             "title" => "Login | " . SITE["name"] 
         ]);
     }
 
-    public function forget()
+    public function forget(): void
     {       
         echo $this->view->render("login/forget" , [
             "title" => "Esqueci a senha | " . SITE["name"] 
         ]);
     }
 
-    public function code()
-    {       
-        echo $this->view->render("login/code" , [
-            "title" => "Confirmar código | " . SITE["name"] 
-        ]);
-    }
-
-    public function newPassword()
+    public function newPassword(): void
     {       
         echo $this->view->render("login/newPassword" , [
             "title" => "Nova Senha | " . SITE["name"] 
