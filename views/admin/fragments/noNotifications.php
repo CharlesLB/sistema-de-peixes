184b2af24f5df023112b58355219775a6fece426
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-06-03 20:18:08 -0300

    fragments

---
+++ views/admin/fragments/noNotifications.php
@@ -0,0 +1,6 @@
+<div class="noNotifications">
+    <p>
+        <i class="fas fa-exclamation-circle"></i>
+        Opa, parecem que todas as mensagens já foram respondidas! Quando houver alguma nova notificação, ela aparecerá aqui.
+    </p>
+</div>
