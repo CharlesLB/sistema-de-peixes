d1e9f92ec195a876a15fc65f21f0afc60bd28c12
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-06-22 12:04:49 -0300

    New Admin

---
+++ views/admin/fragments/widgets/theme/mini-mail.php
@@ -0,0 +1,11 @@
+<a class="dropdown-item d-flex align-items-center" href="#">
+    <div class="mr-3">
+        <div class="icon-circle bg-primary">
+            <i class="fas fa-file-alt text-white"></i>
+        </div>
+    </div>
+    <div>
+        <div class="small text-gray-500">20/12/12 - Fulano de tal</div>
+        <span class="font-weight-bold">Esta é uma mensagem de exemplo!</span>
+    </div>
+</a>
