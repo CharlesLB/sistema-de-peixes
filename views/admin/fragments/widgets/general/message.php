83a62d3905cc3c075c1548c15cae6e64a3ea2481
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-06-22 17:00:07 -0300

    Widgets

---
+++ views/admin/fragments/widgets/general/message.php
@@ -0,0 +1,5 @@
+<div class="card mb-4 py-3 border-bottom-primary">
+    <div class="card-body">
+        <?= $message ?>
+    </div>
+</div>
---
+++ views/admin/fragments/widgets/mails/mail.php
@@ -0,0 +1,12 @@
+<div class="card mb-4">
+    <div class="card-header">
+        <div class="small text-gray-500">20/12/12 - Fulano de tal</div>
+    </div>
+    <div class="card-body">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quasi aliquam dolor reprehenderit mollitia voluptatum praesentium excepturi earum sunt voluptate a dicta laborum voluptatibus, voluptatem eius sequi molestiae labore officiis!
+    </div>
+    <div class="card-footer d-flex justify-content-end">
+        <button type="button" class="btn btn-secondary mr-3">Marcar como respondida</button>
+        <button type="button" class="btn btn-primary">Responder e-mail</button>
+    </div>
+</div>
