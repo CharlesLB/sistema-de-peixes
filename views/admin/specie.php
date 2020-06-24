ca1bef72e5a1f841076e0719faf2bb15f65c5060
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-06-24 16:46:02 -0300

    CSS imported by PHP Test

--- views/admin/specie.php
+++ views/admin/specie.php
@@ -27,23 +27,6 @@
         "icon" => "ruler"  
     ])?>
 
-    <div class="col-xl-6 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1"></div>
-                        <div class="h6 mb-0 font-weight-bold text-gray-800">
-                            
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-ruler fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 
 
